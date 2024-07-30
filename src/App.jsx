63bd90d8199bb5437cdb4257b4597058198da700
Tlalemasego <tlalemasego@gmail.com>
2024-07-30 14:41:38 +0200

    git commit - created header component and added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 
+
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+           <header className="header">
+            <img
+                src = "./images/troll-face.png"
+                className = "header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React</h4>
+        </header>
+    </div>
   )
+   
 }
 
 export default App
