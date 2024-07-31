63c4f9cf3c12240dfaec33c47fd609978015be84
Tlalemasego <tlalemasego@gmail.com>
2024-07-31 14:08:41 +0200

    git commit - Fetched meme data using an API

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import React from "react"
-import memesData from "./memesData.js"
+// Imports the react library
+import React from "react" 
 
+// The meme state holds the the text inputs and the url image
 function App() {
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg"
   })
-  const [allmemeImages, setAllMemeImages] = React.useState(memesData);
+  const [allMemes, setAllMemes] = React.useState([]); // The allmemes state holds the array of memes fetched from the API
 
+  //The React.useEffect runs after the component mounts to fetch the memes. 
+  React.useEffect(() => {
+    async function getMemes(){    //The getMemes function fetches the data from the API, parses the response as JSON and updates the allMemes state with the array of meme objects 
+      const res = await fetch("https://api.imgflip.com/get_memes")
+      const data = await res.json()
+      setAllMemes(data.data.memes)
+    }
+    getMemes()
+  }, [])
+ // The getMemeImage function selects a random meme from the all memes array
   function getMemeImage() {
-    const memesArray = allmemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url
+    const randomNumber = Math.floor(Math.random() * allMemes.length); // It updates the randomImage propety of the meme state with the URL of the selected meme
+    const url = allMemes[randomNumber].url
     setMeme(prevMeme => ({
       ...prevMeme,
       randomImage: url
     }))
   
   }
+
+  // The handleChange function updates the toptext or bottomText properties of the meme state when input is added to the field using the name attribute of the input elements to determine which property to update
   function handleChange(event) {
     const {name, value} = event.target;
     setMeme(prevMeme => ({
@@ -26,9 +38,9 @@ function App() {
       [name]: value
     }))
   }
-
+// The retun statement defines the jsx structure of the component , including a header section with title, description and image and the main section containing a form with text inputs, a buttom to get new meme and div to display the meme images. 
   return (
-    <>
+    <> 
     <div>
         <header className="header">
         <img src = "./images/troll-face.png" className = "header--image"/>
