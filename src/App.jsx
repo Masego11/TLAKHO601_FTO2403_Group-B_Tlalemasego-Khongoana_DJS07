5bf24e76803dfbd8dcede5b0388cbc0eaee6fb2c
Tlalemasego <tlalemasego@gmail.com>
2024-07-31 12:12:15 +0200

    git commit - connected the top and bottom text inputs into state.

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,13 @@ function App() {
     }))
   
   }
+  function handleChange(event) {
+    const {name, value} = event.target;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }))
+  }
 
   return (
     <>
@@ -35,11 +42,17 @@ function App() {
             type="text"
             placeholder="Top text"
             className="form--input"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
             />
           <input
             type="text"
             placeholder="Bottom text"
             className="form--input"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
             />
           <button
             className="form--button"
@@ -48,7 +61,11 @@ function App() {
             Get a new meme image
           </button>
       </div>
+      <div className="meme">
       <img src={meme.randomImage} className="meme--image" />
+      <h2 className="meme--text top">{meme.topText}</h2>
+      <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
     </>
   )
