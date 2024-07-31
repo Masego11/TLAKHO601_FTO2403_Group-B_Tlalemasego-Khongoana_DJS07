39027d1d2e7b9769aa1f9a4bee35d7315526a893
Tlalemasego <tlalemasego@gmail.com>
2024-07-31 09:48:45 +0200

    git commit - Created header and input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-
-
 function App() {
 
   return (
+    <>
     <div>
-           <header className="header">
-            <img
-                src = "./images/troll-face.png"
-                className = "header--image"
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React</h4>
+        <header className="header">
+        <img
+          src = "./images/troll-face.png"
+          className = "header--image"
+        />
+          <h2 className="header--title">Meme Generator</h2>
+          <h4 className="header--project">React</h4>
         </header>
-    </div>
+   </div>
+    <main>
+      <div className="form">
+          <input
+            type="text"
+            placeholder="Top text"
+            className="form--input"
+            />
+            <input
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+            />
+          <button
+            className="form--button"
+            //onClick={getMemeImage}
+          >
+            Get a new meme image
+          </button>
+      </div>
+    </main>
+    </>
   )
    
 }
